fbd26391a590a9ff085069a749bf4e9e57f0a138
Tim Mensch <tim-git-surface@bitgems.com>
2016-05-22 13:35:06 -0600

    Add a copy-merge feature.

--- src/extension.ts
+++ src/extension.ts
@@ -9,40 +9,47 @@ export function activate(context: vscode.ExtensionContext) {
 	let bufSize = 10;
 	var copyBuffer = new Array;
 	var pasteIndex = 0;
-	
+
+	function newCopyBuf(e,merge:boolean=false) : string {
+		let d = e.document;
+		let sel = e.selection;
+		let txt: string = d.getText(new Range(sel.start, sel.end));
+		if (merge) {
+			if (copyBuffer.length===0) {
+				copyBuffer.push("");
+			}
+			copyBuffer[0] += txt;
+		} else {
+			if (txt.trim().length > 0 && !copyBuffer.find(value=> value === txt)) {
+				copyBuffer.unshift(txt);
+				if (copyBuffer.length > bufSize){
+					copyBuffer = copyBuffer.slice(0, bufSize);
+				}
+			}
+		}
+		pasteIndex = 0;
+		return txt;
+	}
+
 	var disposables = [];
 	disposables.push( vscode.commands.registerCommand('multiclip.clearBuffer', () => {
 		copyBuffer = new Array;
 		pasteIndex = 0;
 	}));
-	disposables.push( vscode.commands.registerCommand('multiclip.copy', () => {
-		let e = Window.activeTextEditor;
-		let d = e.document;
-		let sel = e.selection;
-		
-		let txt: string = d.getText(new Range(sel.start, sel.end));
-		if (txt.trim().length > 0 && !copyBuffer.find(value=> value === txt)) {
-			copyBuffer.unshift(txt);
-			if (copyBuffer.length > bufSize){
-				copyBuffer = copyBuffer.slice(0, bufSize);
-			}
-		}
-		pasteIndex = 0;
+	disposables.push( vscode.commands.registerCommand('multiclip.copyMerge', () => {
+		newCopyBuf(Window.activeTextEditor,true);
 		vscode.commands.executeCommand("editor.action.clipboardCopyAction");
 	}));
+	disposables.push( vscode.commands.registerCommand('multiclip.copy', () => {
+		newCopyBuf(Window.activeTextEditor);
+		vscode.commands.executeCommand("editor.action.clipboardCopyAction");
+	}));
+	disposables.push( vscode.commands.registerCommand('multiclip.cutMerge', () => {
+		newCopyBuf(Window.activeTextEditor,true);
+		vscode.commands.executeCommand("editor.action.clipboardCutAction");
+	}));
 	disposables.push( vscode.commands.registerCommand('multiclip.cut', () => {
-		let e = Window.activeTextEditor;
-		let d = e.document;
-		let sel = e.selection;
-		
-		let txt: string = d.getText(new Range(sel.start, sel.end));
-		if (txt.trim().length > 0 && !copyBuffer.find(value=> value === txt)) {
-			copyBuffer.unshift(txt);
-			if (copyBuffer.length > bufSize){
-				copyBuffer = copyBuffer.slice(0, bufSize);
-			}
-		}
-		pasteIndex = 0;
+		newCopyBuf(Window.activeTextEditor);
 		vscode.commands.executeCommand("editor.action.clipboardCutAction");
 	}));
 	disposables.push( vscode.commands.registerCommand('multiclip.paste', () => {
@@ -50,15 +57,15 @@ export function activate(context: vscode.ExtensionContext) {
 			Window.setStatusBarMessage("Multiclip: Nothing to paste", 3000);
 			return;
 		}
-	
+
 		let e = Window.activeTextEditor;
 		let d = e.document;
-		
+
 		let txt: string = d.getText(new Range(e.selection.start, e.selection.end));
 		if (txt === copyBuffer[pasteIndex]) {
 			pasteIndex = ++pasteIndex < copyBuffer.length ? pasteIndex : 0;
 		}
-		
+
 		let sel = e.selections;
 		e.edit( function(edit) {
 			e.selections.forEach(sel => {
@@ -67,22 +74,22 @@ export function activate(context: vscode.ExtensionContext) {
 				//TODO: Put selection in clipboard
 			});
 		});
-		
+
 	}));
 	disposables.push( vscode.commands.registerCommand('multiclip.list', () => {
 		if (copyBuffer.length == 0) {
 			Window.setStatusBarMessage("Multiclip: Nothing to paste", 3000);
 			return;
 		}
-		
+
 		var opts: QuickPickOptions = { matchOnDescription: true, placeHolder: "What to paste" };
 		var items: QuickPickItem[] = [];
-		
-		
+
+
 		for (var i=0; i<copyBuffer.length; i++){
 			items.push({ label: (i+1).toString(), description: copyBuffer[i]});
 		};
-		
+
 		Window.showQuickPick(items).then( (item) => {
 			if (!item) {
 				return;
@@ -90,7 +97,7 @@ export function activate(context: vscode.ExtensionContext) {
 			let e = Window.activeTextEditor;
 			let d = e.document;
 			let sel = e.selections;
-			
+
 			e.edit( function(edit) {
 				e.selections.forEach(sel => {
 					let txt = item.description;
@@ -98,11 +105,11 @@ export function activate(context: vscode.ExtensionContext) {
 					//TODO: Put selection in clipboard
 				});
 			});
-			
+
 		})
 
 	}));
-	
+
 	context.subscriptions.concat(disposables);
 }
 
